8963bc25bdcf3c27b6e2a2eef7b11ea255496f2d
CarlosJHR64 <carlosjhr64@gmail.com>
2021-09-13 15:46:01 -0700

    missed name change

--- zip.sh
+++ zip.sh
@@ -4,7 +4,7 @@ rm -f poltergeist.zip
 mkdir -p ./tmp
 mkdir -p ./tmp/schemas
 cp ./schemas/gschemas.compiled ./tmp/schemas
-cp ./schemas/org.gnome.shell.extensions.bifocals.gschema.xml ./tmp/schemas
+cp ./schemas/org.gnome.shell.extensions.poltergeist.gschema.xml ./tmp/schemas
 cp extension.js ./tmp/
 cp LICENSE ./tmp/
 cp metadata.json ./tmp/
